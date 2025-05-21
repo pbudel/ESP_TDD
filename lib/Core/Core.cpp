23eb2c1943882ad0db8145982bca90316983f5f6
Pierluigi Budel <pbudel@gmail.com>
2025-05-21 08:16:54 +0200

    Example of reusable core function

--- lib/Core/Core.cpp
+++ lib/Core/Core.cpp
@@ -1,4 +1,20 @@
-// This file is just a collection of includes to make PlatformIO happy
-// and ensure all the needed code is included in the build.
+// Collection of reusable functions and utilities
 
-#include "Blinker/Blinker.cpp"
+#include "Core/Core.h"
+
+std::string urlEncode(const std::string &msg)
+{
+    std::ostringstream encoded;
+    for (unsigned char c : msg)
+    {
+        if (isalnum(c) || c == '-' || c == '_' || c == '.' || c == '~')
+        {
+            encoded << c;
+        }
+        else
+        {
+            encoded << '%' << std::uppercase << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(c);
+        }
+    }
+    return encoded.str();
+}
---
+++ lib/Core/Core.h
@@ -0,0 +1,5 @@
+#include <string>
+#include <sstream>
+#include <iomanip>
+
+std::string urlEncode(const std::string &msg);
